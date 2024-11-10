c80619f41a43623fd46bde9b1cedbaecde606d57
bta15 <besha.taher74@gmail.com>
2024-11-10 09:59:24 +0100

    Einen weiteren Unit-Test für einen Service implementiert

--- src/test/kotlin/de/stundenplan/service/SchuleServiceTest.kt
+++ src/test/kotlin/de/stundenplan/service/SchuleServiceTest.kt
@@ -1,5 +1,4 @@
-import de.stundenplan.model.Schule
-import de.stundenplan.model.Schulform
+import de.stundenplan.model.*
 import de.stundenplan.repository.FachRepository
 import de.stundenplan.repository.SchuleRepository
 import de.stundenplan.service.SchuleService
@@ -7,6 +6,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 
 
@@ -17,13 +17,29 @@ class SchuleServiceTest {
 
     @Test
     fun findSchulenOk() {
-        val schule = Schule(1, "Scbule1", "schule-id-1", Schulform.GRUNDSCHULE, emptyList())
+        val schule = Schule(1, "Schule1", "schule-id-1", Schulform.GRUNDSCHULE, emptyList())
+        val klasse = Klasse(1, 1, "1b", schule, emptyList())
+        val fach = Fach(1, "Deutsch")
+        val unterricht = Unterricht(1, 3, fach, klasse)
+        klasse.unterrichtList = listOf(unterricht)
+        schule.klassen = listOf(klasse)
 
         every { schuleRepository.findAll() } returns listOf(schule)
 
         val schulenResult = schuleService.findSchulen()
 
         verify(exactly = 1) { schuleRepository.findAll() }
+
         assertFalse { schulenResult.isEmpty() }
+        assertEquals("Schule1", schulenResult.get(0).schulname)
+        assertEquals("schule-id-1", schulenResult.get(0).schulId)
+
+        assertFalse { schulenResult.get(0).klassen.isEmpty() }
+        assertEquals("1b", schulenResult.get(0).klassen.get(0).bezeichnung)
+        assertEquals(1, schulenResult.get(0).klassen.get(0).klassenstufe)
+
+        assertFalse { schulenResult.get(0).klassen.get(0).unterrichtList.isEmpty() }
+        assertEquals(3, schulenResult.get(0).klassen.get(0).unterrichtList.get(0).anzahlProWoche)
+        assertEquals("Deutsch", schulenResult.get(0).klassen.get(0).unterrichtList.get(0).fach.bezeichnung)
     }
 }
