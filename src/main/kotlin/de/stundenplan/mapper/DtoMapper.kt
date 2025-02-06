c2b225493c5d393530ef538f614667a1d0736435
bta15 <besha.taher74@gmail.com>
2025-02-06 12:16:42 +0100

    Stundenplan-Vorbereitung

--- src/main/kotlin/de/stundenplan/mapper/DtoMapper.kt
+++ src/main/kotlin/de/stundenplan/mapper/DtoMapper.kt
@@ -4,40 +4,43 @@ import de.stundenplan.controller.dto.FachDto
 import de.stundenplan.controller.dto.KlasseDto
 import de.stundenplan.controller.dto.SchuleDto
 import de.stundenplan.controller.dto.UnterrichtDto
-import de.stundenplan.model.Fach
-import de.stundenplan.model.Klasse
-import de.stundenplan.model.Schule
-import de.stundenplan.model.Unterricht
+import de.stundenplan.model.*
+import de.stundenplan.model.enums.FachBezeichnung
+import de.stundenplan.model.enums.Wochentag
 import de.stundenplan.repository.FachRepository
 
 class DtoMapper {
     companion object {
 
         fun getOrCreateEntity(fachDto: FachDto, fachRepository: FachRepository): Fach {
-            val existingFach = fachRepository.findByBezeichnung(fachDto.bezeichnung)
+            val existingFach = fachRepository.findByName(fachDto.bezeichnung)
             if (existingFach.isPresent) {
                 return existingFach.get()
             } else {
-                return fachRepository.save(Fach(null, fachDto.bezeichnung))
+                return fachRepository.save(Fach(null, fachDto.bezeichnung, listOf()))
             }
         }
 
-        fun mapToEntity(unterricht: UnterrichtDto, klasse: Klasse, fachRepository: FachRepository): Unterricht =
-            Unterricht(null, unterricht.anzahlProWoche, getOrCreateEntity(unterricht.fach, fachRepository), klasse)
+//        fun mapToEntity(unterricht: UnterrichtDto, klasse: Klasse, fachRepository: FachRepository): Unterrichtsstunde {
+//            val tag = Tag(null, Wochentag.MONTAG, null, listOf())
+//            return Unterrichtsstunde(null, unterricht.anzahlProWoche, getOrCreateEntity(unterricht.fach, fachRepository), klasse.klassenstufe, tag)
+//        }
+
 
 
         fun mapToEntity(klasseDto: KlasseDto, schule: Schule, fachRepository: FachRepository): Klasse {
             val klasse = Klasse(null, klasseDto.klassenstufe, klasseDto.bezeichnung, schule, listOf())
-            val unterrichtList =
-                klasseDto.unterrichtList.map { unterricht -> mapToEntity(unterricht, klasse, fachRepository) }
-            klasse.unterrichtList = unterrichtList
+//            val unterrichtList =
+//                klasseDto.unterrichtList.map { unterricht -> mapToEntity(unterricht, klasse, fachRepository) }
+//            klasse.unterrichtList = unterrichtList
             return klasse
         }
 
         fun mapToEntity(schuleDto: SchuleDto, fachRepository: FachRepository): Schule {
-            val schule = Schule(null, schuleDto.schulname, schuleDto.schulId, schuleDto.schulform, listOf())
-            val klassen = schuleDto.klassen.map { klasse -> mapToEntity(klasse, schule, fachRepository) }
-            schule.klassen = klassen
+            val schule = Schule(null, schuleDto.schulname, schuleDto.schulId, schuleDto.schulform)
+//            schuleDto.klassen
+//            val klassen = schuleDto.klassen.map { klasse -> mapToEntity(klasse, schule, fachRepository) }
+//            schule.klassen = klassen
             return schule
         }
     }
--- src/main/kotlin/de/stundenplan/mapper/EntityMapper.kt
+++ src/main/kotlin/de/stundenplan/mapper/EntityMapper.kt
@@ -7,23 +7,24 @@ import de.stundenplan.controller.dto.UnterrichtDto
 import de.stundenplan.model.Fach
 import de.stundenplan.model.Klasse
 import de.stundenplan.model.Schule
-import de.stundenplan.model.Unterricht
+import de.stundenplan.model.Unterrichtsstunde
 
 class EntityMapper {
     companion object {
-        fun mapToDto(fach: Fach): FachDto = FachDto(fach.bezeichnung)
+//        fun mapToDto(fach: Fach): FachDto = FachDto(fach.bezeichnung)
 
-        fun mapToDto(unterricht: Unterricht): UnterrichtDto =
-            UnterrichtDto(unterricht.anzahlProWoche, mapToDto(unterricht.fach))
+//        fun mapToDto(unterrichtsstunde: Unterrichtsstunde): UnterrichtDto =
+//            UnterrichtDto(unterrichtsstunde.anzahlProWoche, mapToDto(unterrichtsstunde.fach))
 
-        fun mapToDto(klasse: Klasse): KlasseDto {
-            val unterrichtDtos = klasse.unterrichtList.map { unterricht -> mapToDto(unterricht) }
-            return KlasseDto(klasse.klassenstufe, klasse.bezeichnung, unterrichtDtos)
-        }
+//        fun mapToDto(klasse: Klasse): KlasseDto {
+//            val unterrichtDtos = klasse.unterrichtList.map { unterricht -> mapToDto(unterricht) }
+//            return KlasseDto(klasse.klassenstufe, klasse.bezeichnung, unterrichtDtos)
+//        }
 
         fun mapToDto(schule: Schule): SchuleDto {
-            val klasseDtos = schule.klassen.map { klasse -> mapToDto(klasse) }
-            return SchuleDto(schule.schulname, schule.schulId, schule.schulform, klasseDtos)
+//            val klasseDtos = schule.klassen.map { klasse -> mapToDto(klasse) }
+
+            return SchuleDto(schule.schulname, schule.schulId, schule.schulform)
         }
 
         fun mapToDto(schulen: List<Schule>): List<SchuleDto> = schulen.map { schule -> mapToDto(schule) }
